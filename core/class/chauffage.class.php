07c3f12211319e214e20cc9c813f0a76833f1c05
ktn <jeedom@7ea6d355132c>
2022-12-30 00:56:13 +0100

    création de la commande 'consigne'

--- core/class/chauffage.class.php
+++ core/class/chauffage.class.php
@@ -19,151 +19,161 @@
 require_once __DIR__  . '/../../../../core/php/core.inc.php';
 
 class chauffage extends eqLogic {
-  /*     * *************************Attributs****************************** */
+	/*     * *************************Attributs****************************** */
 
-  /*
-  * Permet de définir les possibilités de personnalisation du widget (en cas d'utilisation de la fonction 'toHtml' par exemple)
-  * Tableau multidimensionnel - exemple: array('custom' => true, 'custom::layout' => false)
-  public static $_widgetPossibility = array();
-  */
+	/*
+	* Permet de définir les possibilités de personnalisation du widget (en cas d'utilisation de la fonction 'toHtml' par exemple)
+	* Tableau multidimensionnel - exemple: array('custom' => true, 'custom::layout' => false)
+	public static $_widgetPossibility = array();
+	*/
 
-  /*
-  * Permet de crypter/décrypter automatiquement des champs de configuration du plugin
-  * Exemple : "param1" & "param2" seront cryptés mais pas "param3"
-  public static $_encryptConfigKey = array('param1', 'param2');
-  */
+	/*
+	* Permet de crypter/décrypter automatiquement des champs de configuration du plugin
+	* Exemple : "param1" & "param2" seront cryptés mais pas "param3"
+	public static $_encryptConfigKey = array('param1', 'param2');
+	*/
 
-  /*     * ***********************Methode static*************************** */
+	/*     * ***********************Methode static*************************** */
 
-  /*
-  * Fonction exécutée automatiquement toutes les minutes par Jeedom
-  public static function cron() {}
-  */
+	/*
+	* Fonction exécutée automatiquement toutes les minutes par Jeedom
+	public static function cron() {}
+	*/
 
-  /*
-  * Fonction exécutée automatiquement toutes les 5 minutes par Jeedom
-  public static function cron5() {}
-  */
+	/*
+	* Fonction exécutée automatiquement toutes les 5 minutes par Jeedom
+	public static function cron5() {}
+	*/
 
-  /*
-  * Fonction exécutée automatiquement toutes les 10 minutes par Jeedom
-  public static function cron10() {}
-  */
+	/*
+	* Fonction exécutée automatiquement toutes les 10 minutes par Jeedom
+	public static function cron10() {}
+	*/
 
-  /*
-  * Fonction exécutée automatiquement toutes les 15 minutes par Jeedom
-  public static function cron15() {}
-  */
+	/*
+	* Fonction exécutée automatiquement toutes les 15 minutes par Jeedom
+	public static function cron15() {}
+	*/
 
-  /*
-  * Fonction exécutée automatiquement toutes les 30 minutes par Jeedom
-  public static function cron30() {}
-  */
+	/*
+	* Fonction exécutée automatiquement toutes les 30 minutes par Jeedom
+	public static function cron30() {}
+	*/
 
-  /*
-  * Fonction exécutée automatiquement toutes les heures par Jeedom
-  public static function cronHourly() {}
-  */
+	/*
+	* Fonction exécutée automatiquement toutes les heures par Jeedom
+	public static function cronHourly() {}
+	*/
 
-  /*
-  * Fonction exécutée automatiquement tous les jours par Jeedom
-  public static function cronDaily() {}
-  */
+	/*
+	* Fonction exécutée automatiquement tous les jours par Jeedom
+	public static function cronDaily() {}
+	*/
 
-  /*     * *********************Méthodes d'instance************************* */
+	/*     * *********************Méthodes d'instance************************* */
 
-  // Fonction exécutée automatiquement avant la création de l'équipement
-  public function preInsert() {
-  }
+	// Fonction exécutée automatiquement avant la création de l'équipement
+	public function preInsert() {
+	}
 
-  // Fonction exécutée automatiquement après la création de l'équipement
-  public function postInsert() {
-  }
+	// Fonction exécutée automatiquement après la création de l'équipement
+	public function postInsert() {
+		$cmd = new chauffageCmd();
+		$cmd->setEqLogic_id($this->getId());
+		$cmd->setName('consigne');
+		$cmd->setType('info');
+		$cmd->setSubType('numeric');
+		$cmd->setLogicalId('consigne');
+		$cmd->setConfiguration('minValue',0);
+		$cmd->setConfiguration('maxValue',25);
+		$cmd->setUnite('°C');
+		$cmd->save();
+	}
 
-  // Fonction exécutée automatiquement avant la mise à jour de l'équipement
-  public function preUpdate() {
-  }
+	// Fonction exécutée automatiquement avant la mise à jour de l'équipement
+	public function preUpdate() {
+	}
 
-  // Fonction exécutée automatiquement après la mise à jour de l'équipement
-  public function postUpdate() {
-  }
+	// Fonction exécutée automatiquement après la mise à jour de l'équipement
+	public function postUpdate() {
+	}
 
-  // Fonction exécutée automatiquement avant la sauvegarde (création ou mise à jour) de l'équipement
-  public function preSave() {
-  }
+	// Fonction exécutée automatiquement avant la sauvegarde (création ou mise à jour) de l'équipement
+	public function preSave() {
+	}
 
-  // Fonction exécutée automatiquement après la sauvegarde (création ou mise à jour) de l'équipement
-  public function postSave() {
-  }
+	// Fonction exécutée automatiquement après la sauvegarde (création ou mise à jour) de l'équipement
+	public function postSave() {
+	}
 
-  // Fonction exécutée automatiquement avant la suppression de l'équipement
-  public function preRemove() {
-  }
+	// Fonction exécutée automatiquement avant la suppression de l'équipement
+	public function preRemove() {
+	}
 
-  // Fonction exécutée automatiquement après la suppression de l'équipement
-  public function postRemove() {
-  }
+	// Fonction exécutée automatiquement après la suppression de l'équipement
+	public function postRemove() {
+	}
 
-  /*
-  * Permet de crypter/décrypter automatiquement des champs de configuration des équipements
-  * Exemple avec le champ "Mot de passe" (password)
-  public function decrypt() {
-    $this->setConfiguration('password', utils::decrypt($this->getConfiguration('password')));
-  }
-  public function encrypt() {
-    $this->setConfiguration('password', utils::encrypt($this->getConfiguration('password')));
-  }
-  */
+	/*
+	* Permet de crypter/décrypter automatiquement des champs de configuration des équipements
+	* Exemple avec le champ "Mot de passe" (password)
+	public function decrypt() {
+	  $this->setConfiguration('password', utils::decrypt($this->getConfiguration('password')));
+	}
+	public function encrypt() {
+	  $this->setConfiguration('password', utils::encrypt($this->getConfiguration('password')));
+	}
+	*/
 
-  /*
-  * Permet de modifier l'affichage du widget (également utilisable par les commandes)
-  public function toHtml($_version = 'dashboard') {}
-  */
+	/*
+	* Permet de modifier l'affichage du widget (également utilisable par les commandes)
+	public function toHtml($_version = 'dashboard') {}
+	*/
 
-  /*
-  * Permet de déclencher une action avant modification d'une variable de configuration du plugin
-  * Exemple avec la variable "param3"
-  public static function preConfig_param3( $value ) {
-    // do some checks or modify on $value
-    return $value;
-  }
-  */
+	/*
+	* Permet de déclencher une action avant modification d'une variable de configuration du plugin
+	* Exemple avec la variable "param3"
+	public static function preConfig_param3( $value ) {
+	  // do some checks or modify on $value
+	  return $value;
+	}
+	*/
 
-  /*
-  * Permet de déclencher une action après modification d'une variable de configuration du plugin
-  * Exemple avec la variable "param3"
-  public static function postConfig_param3($value) {
-    // no return value
-  }
-  */
+	/*
+	* Permet de déclencher une action après modification d'une variable de configuration du plugin
+	* Exemple avec la variable "param3"
+	public static function postConfig_param3($value) {
+	  // no return value
+	}
+	*/
 
-  /*     * **********************Getteur Setteur*************************** */
+	/*     * **********************Getteur Setteur*************************** */
 
 }
 
 class chauffageCmd extends cmd {
-  /*     * *************************Attributs****************************** */
+	/*     * *************************Attributs****************************** */
 
-  /*
-  public static $_widgetPossibility = array();
-  */
+	/*
+	public static $_widgetPossibility = array();
+	*/
 
-  /*     * ***********************Methode static*************************** */
+	/*     * ***********************Methode static*************************** */
 
 
-  /*     * *********************Methode d'instance************************* */
+	/*     * *********************Methode d'instance************************* */
 
-  /*
-  * Permet d'empêcher la suppression des commandes même si elles ne sont pas dans la nouvelle configuration de l'équipement envoyé en JS
-  public function dontRemoveCmd() {
-    return true;
-  }
-  */
+	/*
+	* Permet d'empêcher la suppression des commandes même si elles ne sont pas dans la nouvelle configuration de l'équipement envoyé en JS
+	public function dontRemoveCmd() {
+	  return true;
+	}
+	*/
 
-  // Exécution d'une commande
-  public function execute($_options = array()) {
-  }
+	// Exécution d'une commande
+	public function execute($_options = array()) {
+	}
 
-  /*     * **********************Getteur Setteur*************************** */
+	/*     * **********************Getteur Setteur*************************** */
 
 }
